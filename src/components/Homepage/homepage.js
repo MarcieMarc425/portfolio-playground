458d0aabc2b965bff266c17d91b7375efbeeba6a
Chun Zen Marc Tse <32941251+MarcieMarc425@users.noreply.github.com>
2019-02-20 12:34:13 -0500

    Added homepage

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -3,16 +3,24 @@ import heroVid from '../../resources/hero.mp4';
 import './homepage.css';
 
 export default class homepage extends Component {
-	render() {
-		return (
-			<div className='homepage'>
-				<div className='heroCover' />
-				<div className='hero'>
-					<video id='heroVid' autoPlay muted loop>
-						<source src={heroVid} type='video/mp4' />
-					</video>
-				</div>
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className='homepage'>
+                <div className='heroCover'>
+                    <div className='heroText'>Marc Tse</div>
+                    <div className='heroText2'>A DEVELOPER</div>
+                    <div className='about'>About</div>
+                </div>
+                <div className='hero'>
+                    <video id='heroVid' autoPlay muted loop>
+                        <source src={heroVid} type='video/mp4' />
+                    </video>
+                </div>
+            </div>
+        );
+    }
 }
